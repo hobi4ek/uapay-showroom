06fb9b49b0b6bcdf5bdefbec7f40b0957391614b
Yurii Ivanov <yurii.ivanov@uapay.ua>
2019-02-05 15:25:31 +0200

    init

---
+++ src/app/distributed-payments/distributed-payments.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+	selector: 'app-distributed-payments',
+	templateUrl: './distributed-payments.component.html',
+	styleUrls: ['./distributed-payments.component.scss']
+})
+export class DistributedPaymentsComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit() {
+	}
+
+}
